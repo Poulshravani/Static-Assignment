f0ea276f71ab8196e850128e64950c327506901e
Poulshravani <poulshravani2@gmail.com>
2025-05-09 02:39:18 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Static_Assignment
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+
+        {
+            int lintA, lintB;
+
+            Console.WriteLine("Enter value for a");
+            lintA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value for b");
+            lintB = Convert.ToInt32(Console.ReadLine());
+
+
+            Simplecode lobj1 = new Simplecode();
+            int lintSum = lobj1.Addition(lintA, lintB);
+            lobj1.PrintValues();
+
+            Simplecode lobj2 = new Simplecode();
+            int lintSub = lobj2.Substract(lintA, lintB);
+            lobj2.PrintValues();
+
+            Console.WriteLine("Addition =" + lintSum);
+            Console.WriteLine("Subtraction =" + lintSum);
+
+
+
+            
+            Simplecode lobj = new Simplecode();
+
+            Console.Write("Enter name:");
+            lobj1.Name = Console.ReadLine();
+
+            Console.Write("Enter age: ");
+            lobj.Age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nStudent Details:");
+            lobj.ShowInfo();
+
+            
+
+
+
+
+        }
+
+    }
+}         
+        
---
+++ Simplecode.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Static_Assignment
+{
+    public class Simplecode
+    {
+        
+        
+            private int result;
+
+            public int Addition(int a, int b)
+            {
+                result = a + b;
+                return result;
+            }
+
+            public int Substract(int a, int b)
+            {
+                result = a - b;
+                return result;
+            }
+
+            public void PrintValues()
+            {
+                Console.WriteLine("Result: " + result);
+            }
+
+            public string Name="";
+            public int Age;
+            public void ShowInfo()
+            {
+                Console.WriteLine("Name:" + Name);
+                Console.WriteLine("Age:" + Age);
+            
+
+            }
+
+
+
+
+
+    }
+}
